f04b64e08b83712b17a9d675f03e3bd97df2b9d9
andrei <andrei.ghita2002@gmail.com>
2023-01-22 17:42:01 +0000

    started work on more main menu buttons

--- src/java/jchess/ux/MainMenuScene.java
+++ src/java/jchess/ux/MainMenuScene.java
@@ -6,8 +6,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class MainMenuScene extends Scene {
     static Group root;
+    static String scenariosPath = "/Users/andrei/IdeaProjects/JChess/src/resources/scenarios";
 
     public MainMenuScene(Parent parent, Theme theme) {
         super(parent, Controller.H, Controller.W);
@@ -24,6 +29,23 @@ public class MainMenuScene extends Scene {
         Button playClassicButton = new Button("Play Classic");
         playClassicButton.setOnAction((e) -> Controller.changeScene(GUIStates.GAME_BOARD, "classic"));
         vBox.getChildren().add(playClassicButton);
+        
+        // Pre-made Scenarios Button
+        Button preMadeButton = new Button("Load Scenario");
+        preMadeButton.setOnAction((e) -> {
+            try {
+                System.out.println(Files.list(Path.of(scenariosPath)).toList());
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        });
+        vBox.getChildren().add(preMadeButton);
+        
+        // Import Scenario Button
+        //TODO import scenarios feature
+        Button importButton = new Button("Import Button");
+        importButton.setOnAction((e) -> System.out.println("!! import button was pressed"));
+        vBox.getChildren().add(importButton);
 
         // Quit Button
         Button quitButton = new Button("Quit");
